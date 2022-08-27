d5ed62865e1e6fccbe5d8c7fba2f1abf25dd2123
Stefano Dolci <ste.dolci16@gmail.com>
2022-08-27 13:21:59 +0200

    create mysql connection

---
+++ feedback/config/database.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'admin');
+define('DB_PASS', 'password');
+define('DB_NAME', 'php_dev');
+
+// Create Connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Check connection
+if($conn->connect_error) {
+    die('Connection Failed' . $conn->connect_error);
+}
--- feedback/inc/header.php
+++ feedback/inc/header.php
@@ -1,5 +1,8 @@
+<?php include 'config/database.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <title>Leave Feedback</title>
 </head>
+
 <body>
   <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
     <div class="container">
@@ -16,23 +20,19 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-         <li class="nav-item">
-              <a class="nav-link" href="/Applications/XAMPP/xamppfiles/htdocs/php-crash/feedback/index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/Applications/XAMPP/xamppfiles/htdocs/php-crash/feedback/feedback.html"
-                >Feedback</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/Applications/XAMPP/xamppfiles/htdocs/php-crash/feedback/about.html"
-                >About</a
-              >
-            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/php-crash/feedback/index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/php-crash/feedback/feedback.php">Feedback</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/php-crash/feedback/about.php">About</a>
+          </li>
         </ul>
       </div>
-  </div>
-</nav>
+    </div>
+  </nav>
 
-<main>
-  <div class="container d-flex flex-column align-items-center">
+  <main>
+    <div class="container d-flex flex-column align-items-center">
